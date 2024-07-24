08e2002e5e4eb8e2a4ed6cb109222a9c875dba1f
Janmeyjai Mishra <mishrajanmeyjai100@gmail.com>
2024-07-24 19:00:51 +0530

    Done login page design

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 import svg from "../../../public/real_logo.svg";
 
 export const metadata = {
-  title: "Signup",
+  title: "Login",
 };
 
 function Page() {
@@ -22,7 +22,7 @@ function Page() {
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-              Create an account
+              Login to your account
             </h1>
             <form className="space-y-4 md:space-y-6" action="#">
               <div>
@@ -57,60 +57,21 @@ function Page() {
                   required=""
                 />
               </div>
-              <div>
-                <label
-                  htmlFor="confirm-password"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Confirm password
-                </label>
-                <input
-                  type="confirm-password"
-                  name="confirm-password"
-                  id="confirm-password"
-                  placeholder="••••••••"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required=""
-                />
-              </div>
-              <div className="flex items-start">
-                <div className="flex items-center h-5">
-                  <input
-                    id="terms"
-                    aria-describedby="terms"
-                    type="checkbox"
-                    className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                    required=""
-                  />
-                </div>
-                <div className="ml-3 text-sm">
-                  <label
-                    htmlFor="terms"
-                    className="font-light text-gray-500 dark:text-gray-300"
-                  >
-                    I accept the{" "}
-                    <a
-                      className="font-medium text-blue-700 hover:underline dark:text-primary-500"
-                      href="#"
-                    >
-                      Terms and Conditions
-                    </a>
-                  </label>
-                </div>
-              </div>
+              <div></div>
+
               <button
                 type="submit"
                 className="w-full text-white bg-blue-700 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
               >
-                Create an account
+                Login
               </button>
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                Already have an account?{" "}
+                Don&apos;t have an account?{" "}
                 <Link
                   href="/login"
                   className="font-medium text-blue-700 hover:underline dark:text-primary-500"
                 >
-                  Login here
+                  Signup here
                 </Link>
               </p>
             </form>
--- src/app/signin/page.js
+++
@@ -1,5 +0,0 @@
-function Page() {
-  return <div>login</div>;
-}
-
-export default Page;
