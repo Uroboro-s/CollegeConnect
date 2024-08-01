ed8b95b03ea839d5235d495dbb295f151f5bfbfa
Janmeyjai Mishra <mishrajanmeyjai100@gmail.com>
2024-08-01 13:08:02 +0530

    Done with upcoming events function

--- src/app/_lib/data_service.js
+++ src/app/_lib/data_service.js
@@ -1,4 +1,5 @@
 import { supabase } from "./supabase";
+import { isPast } from "date-fns";
 
 //Users are uniquely identified by their emails
 export async function getUser(email) {
@@ -27,14 +28,28 @@ export async function createUser(name, email, image) {
 
   if (error) {
     console.error(error);
-    throw new Error("User could not be created");
+    throw new Error("User could not be created!");
   }
 
   return data;
 }
 
-export async function getEvents() {
-  let { data, error } = await supabase.from("Event").select("*");
+export async function getUpcomingEvents() {
+  const currentDateInMilliseconds = Date.now();
+
+  let currentDate = new Date(currentDateInMilliseconds).toISOString();
+  console.log(currentDate);
+
+  let { data, error } = await supabase
+    .from("Event")
+    .select("*")
+    .gte("start_date", currentDate)
+    .order("start_date");
+
+  if (error) {
+    console.log(error);
+    throw new Error("Upcoming events failed to fetch!");
+  }
 
   return data;
 }
--- src/app/v1/home/page.js
+++ src/app/v1/home/page.js
@@ -1,35 +1,34 @@
 import EventSlider from "@/app/_components/EventSlider";
 import { auth } from "@/app/_lib/auth";
-import { getEvents } from "@/app/_lib/data_service";
+import { getUpcomingEvents } from "@/app/_lib/data_service";
 
 export const metadata = {
   title: "Home",
 };
 
 async function Page() {
-  const events = await getEvents();
+  const events = await getUpcomingEvents();
 
-  // console.log(events);
+  console.log(events);
 
   const sports = events.filter((event) =>
     event.category === "Sports" ? true : false
   ); //done
   const hackathons = events.filter((event) =>
     event.category === "Hackathon" ? true : false
-  );
+  ); //done
   const workshops = events.filter((event) =>
     event.category === "Workshop" ? true : false
-  );
+  ); //done
   const games = events.filter((event) =>
     event.category === "Games" ? true : false
-  );
+  ); //done
   const entertainment = events.filter((event) =>
     event.category === "Entertainment" ? true : false
-  );
+  ); //done
   const learning = events.filter((event) =>
     event.category === "Learning" ? true : false
-  );
-  // console.log(sports);
+  ); //done
 
   return (
     <div className="w-full">
@@ -38,6 +37,7 @@ async function Page() {
       <EventSlider category="Entertainment" events={entertainment} />
       <EventSlider category="Learning" events={learning} />
       <EventSlider category="Hackathons" events={hackathons} />
+      <EventSlider category="Workshops" events={workshops} />
       <EventSlider category="Games" events={games} />
     </div>
   );
